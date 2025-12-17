5fc767841b324d31c7ee3cedb18fc1a778b2605d
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2025-12-18 00:11:35 +0300

    os selection added

--- booststrap.sh
+++ booststrap.sh
@@ -66,6 +66,21 @@ if [ "$1" == "publish" ]; then
   # cd build
   # cpack --config CPackConfig.cmake 
   # cd ..
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # ...
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+          # Mac OSX
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+          # POSIX compatibility layer and Linux environment emulation for Windows
+  elif [[ "$OSTYPE" == "msys" ]] || [ "$OSTYPE" == "win32" ]]; then
+          # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+  elif [[ "$OSTYPE" == "win32" ]]; then
+          # I'm not sure this can happen.
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+          # ...
+  else
+          # Unknown.
+  fi
 
 fi
 
